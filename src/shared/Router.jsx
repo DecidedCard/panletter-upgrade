ab28fccbb7f62c70ac599a4004e86830a5c036ac
DecidedCard <qube7089@naver.com>
2024-02-19 12:12:54 +0900

    update: login page router setting

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,15 +1,17 @@
 import Detail from "pages/Detail";
 import Home from "pages/Home";
 import Letter from "pages/Letter";
+import Login from "pages/Login";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 const Router = () => {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="detail/:id" element={<Detail />} />
         <Route path="letter/:id" element={<Letter />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
