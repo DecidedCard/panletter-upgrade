464f374931a26a5029c571cf9d1eea9382429fdb
DecidedCard <qube7089@naver.com>
2024-02-19 12:13:56 +0900

    refactor: redux toolkit 적용

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,11 +1,17 @@
-import { createStore, combineReducers } from "redux";
+// import { createStore, combineReducers } from "redux";
 import letters from "../modules/letters";
-import { devToolsEnhancer } from "redux-devtools-extension";
+// import { devToolsEnhancer } from "redux-devtools-extension";
+import { configureStore } from "@reduxjs/toolkit";
 
-const rootReducer = combineReducers({
-  letters,
+// const rootReducer = combineReducers({
+//   letters,
+// });
+
+// const store = createStore(rootReducer, devToolsEnhancer());
+const store = configureStore({
+  reducer: {
+    letters,
+  },
 });
-
-const store = createStore(rootReducer, devToolsEnhancer());
 
 export default store;
